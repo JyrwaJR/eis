824bd61f60b7539757a0c6c4cd870111d3863636
JyrwaJR <jyrwaboys@gmail.com>
2026-07-30 13:19:42 +0530

    chore: remove unused card exports from salary feature barrel

--- src/features/salary/components/index.ts
+++ src/features/salary/components/index.ts
@@ -1,8 +1,1 @@
 export * from './skeleton';
-
-export * from './employee-info-card';
-export * from './gpf-info-card';
-export * from './bank-voucher-card';
-export * from './salary-breakdown-card';
-export * from './totals-card';
-export * from './net-pay-words-card';
