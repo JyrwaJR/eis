0a9dc2487fbc81b3c79b34eeb8d81658706286d0
JyrwaJR <jyrwaboys@gmail.com>
2026-08-04 10:54:30 +0530

    refactor: reuse e-pay-slip payload for query key

--- src/features/e-pay-slip/hooks/use-e-payslip.ts
+++ src/features/e-pay-slip/hooks/use-e-payslip.ts
@@ -9,14 +9,14 @@ export function useEPayslip({ geNumber }: { geNumber: string }) {
   const isGeNumberExist = !!user?.ge_no;
   const enabled = !!isGeNumberExist || !!geNumber;
 
+  const payload: { ge_number: string } = { ge_number: user?.ge_no || geNumber };
+
   return useQuery({
-    queryKey: QUERY_KEYS.E_PAY_SLIP.LIST(isGeNumberExist ? user?.ge_no : geNumber),
+    queryKey: QUERY_KEYS.E_PAY_SLIP.LIST(payload),
     queryFn: () =>
       axiosInstanceWithoutEncryption.post<{ data: EPayslip }>(
         'http://10.179.35.51:82/api/get/epayslip',
-        {
-          ge_number: user?.ge_no || geNumber,
-        }
+        payload
       ),
     select: (data) => data.data.data,
     enabled: enabled && isSignedIn,
